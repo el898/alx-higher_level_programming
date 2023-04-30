e49e260da2b28c824a4ecb016983e3a8e0f3a8a9
el898 <ebubeexcel3@gmail.com>
2023-04-30 13:44:21 -0700

    correct error

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,8 +4,8 @@
 
 class Square:
     """for the square"""
-    
-    def __init__(self,size=0):
+
+    def __init__(self, size=0):
         """
         Args:
             size(int): size of the new square.
@@ -15,10 +15,9 @@ class Square:
             ValueError: if size < 0.
         """
 
-        if not isinstance(size,int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if  size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
 
         self.__size = size
-
