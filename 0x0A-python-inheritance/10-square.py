ff7b76b4cc649c696803f4e1b13934cc6e666dc8
el898 <ebubeexcel3@gmail.com>
2023-05-02 21:11:33 +0100

    inherits from a triangle

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Defines a Rectangle subclass Square."""
+""" class Square that inherits from Rectangle (9-rectangle.py"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square."""
+    """a square class."""
 
     def __init__(self, size):
         """Initialize a new square.
 
         Args:
-            size (int): The size of the new square.
+            size: integer size of the square.
         """
         self.integer_validator("size", size)
         super().__init__(size, size)
