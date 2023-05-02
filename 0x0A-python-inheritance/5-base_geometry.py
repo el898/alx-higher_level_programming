ecb84f101c6a0a0c2510b324bd7e49059a806322
el898 <ebubeexcel3@gmail.com>
2023-05-02 20:43:57 +0100

    base geometry

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Defines an empty class BaseGeometry."""
+""" an empty class BaseGeometry."""
 
 
 class BaseGeometry:
-    """Represent base geometry."""
+    """the base geometry class"""
     pass
