c3d8cafc1d33c8f9b9b1c442247a44f4d4c0d86d
el898 <ebubeexcel3@gmail.com>
2023-05-02 08:52:13 -0700

    divides matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,9 +8,13 @@ def matrix_divided(matrix, div):
         matrix (list): the list of lists of ints or floats.
         div (int/float): The divisor
     """
-    if not all(isinstance(row, list) for row in matrix) or not all(
-            isinstance(num, (int, float)) for row in matrix for num in row):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+     if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((isinstance(ele, int) or isinstance(ele, float))
+                    for ele in [num for row in matrix for num in row])):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
+
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
