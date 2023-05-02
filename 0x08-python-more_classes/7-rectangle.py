c549d8a8aa9544c8c3526b01ea8f43070f45ee3b
el898 <ebubeexcel3@gmail.com>
2023-05-02 09:41:27 +0000

    rectangle

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -62,7 +62,7 @@ ass Rectangle:
             return ("")
         rect = []
         for i in range(self.__height):
-            [rect.append(type(self).print_symbol) for j in range(self.__width)]
+            [rect.append(self.print_symbol) for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
