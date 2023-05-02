0b73e4a20d918d45446a452e30a9d788fa26f881
el898 <ebubeexcel3@gmail.com>
2023-05-02 22:25:22 +0100

    a function that inserts a line of text to a file

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-"""Defines a text file insertion function."""
+"""a function that inserts a line of text to a file"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """Insert text after each line containing a given string in a file.
-    Args:
-        filename (str): The name of the file.
-        search_string (str): The string to search for within the file.
-        new_string (str): The string to insert.
-    """
+    """Insert text after lines"""
     text = ""
     with open(filename) as r:
         for line in r:
