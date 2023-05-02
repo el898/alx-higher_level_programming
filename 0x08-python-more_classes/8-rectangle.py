ce69f193c8f2f9b7f7427f88fb9f8a53c815c358
el898 <ebubeexcel3@gmail.com>
2023-05-02 11:09:17 +0000

    rectangle

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -68,8 +68,7 @@ class Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
 
         if(rect_2.area() > rect_1.area()):
-            return rect_2
-        
+            return rect_2  
         return rect_1
 
     def __str__(self):
