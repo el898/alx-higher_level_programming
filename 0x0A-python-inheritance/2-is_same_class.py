6e44aee67d2fbcf303de59ed6968219c8037f2b9
el898 <ebubeexcel3@gmail.com>
2023-05-02 20:11:39 +0100

    class checking

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Defines a class-checking function."""
+""" a  function that checks claSS"""
 
 
 def is_same_class(obj, a_class):
-    """Check if an object is exactly an instance of a given class.
+    """CheckS if  object is  an instance of a class.
 
     Args:
-        obj (any): The object to check.
-        a_class (type): The class to match the type of obj to.
+        obj : The object
+        a_class (type): The given class to match.
     Returns:
-        If obj is exactly an instance of a_class - True.
-        Otherwise - False.
+        true: If obj is exactly an instance of the specified a_class
+        false: else
     """
     if type(obj) == a_class:
         return True
