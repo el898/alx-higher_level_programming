0f9104511e452393e7a719d6df3e60a305679bff
el898 <ebubeexcel3@gmail.com>
2023-05-02 21:52:30 +0100

    a JSON file

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-''' function that returns a python object represented by a JSON string
+''' function that returns a python object 
 '''
 
 import json
 
 
 def from_json_string(my_str):
-    ''' module from_json_string
+    '''
      returns Python objects
     '''
     return json.loads(my_str)
