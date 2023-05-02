b1d7f24d98b6996a3387fde92fe46689288eb22c
el898 <ebubeexcel3@gmail.com>
2023-05-02 21:20:27 +0100

    MyInt has == and != operators inverted

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Defines a class MyInt that inherits from int."""
+""" a class MyInt that inherits from int."""
 
 
 class MyInt(int):
-    """Invert int operators == and !=."""
+    """MyInt has == and != operators inverted"""
 
     def __eq__(self, value):
-        """Override == opeartor with != behavior."""
+        """Override == opeartor with !=)"""
         return self.real != value
 
     def __ne__(self, value):
-        """Override != operator with == behavior."""
+        """Override != operator with =="""
         return self.real == value
