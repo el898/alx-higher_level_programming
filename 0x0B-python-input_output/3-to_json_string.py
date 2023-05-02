f465673d381b414b30e29a48c3aeccb52810de0e
el898 <ebubeexcel3@gmail.com>
2023-05-02 21:50:55 +0100

    a JSON file

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-''' function that returns the JSON representation of an object (string)
-'''
+''' function that returns the JSON representation of an object (string)'''
 
 import json
 
 
 def to_json_string(my_obj):
-    ''' module to_json_strin
-     returns JSON representation
+    ''' 
+    represnted by JSON strings
     '''
     return json.dumps(my_obj)
