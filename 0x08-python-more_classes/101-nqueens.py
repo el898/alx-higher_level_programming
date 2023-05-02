9357da13f187ae20253029984e8d122650f93a76
el898 <ebubeexcel3@gmail.com>
2023-05-02 14:00:08 +0000

    nqueens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -11,3 +11,11 @@ if __name__ == "__main__":
         print("Usage: nqueens N")
         exit(1)
 
+    n = arguments[1]
+    if (not isinstance(n, int)):
+        print("N must be a number")
+        exit(1)
+
+    if (n < 4):
+        print("N must be at least 4")
+        exit(1)
