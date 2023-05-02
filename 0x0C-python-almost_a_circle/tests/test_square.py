5539f00acc57f7eace9202e9465f53f52680d6e4
el898 <ebubeexcel3@gmail.com>
2023-05-02 22:53:24 +0100

    test square

--- 0x0C-python-almost_a_circle/tests/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -1,18 +1,5 @@
 #!/usr/bin/python3>
-"""Defines unittests for models/square.py.
-
-Unittest classes:
-    TestSquare_instantiation - line 24
-    TestSquare_size - line 88
-    TestSquare_x - line 166
-    TestSquare_y - line 238
-    TestSquare_order_of_initialization - line 306
-    TestSquare_area - line 322
-    TestSquare_stdout - line 343
-    TestSquare_update_args - line 426
-    TestSquare_update_kwargs - line 538
-    TestSquare_to_dictionary - 640
-"""
+"""Defines unittests for models"""
 import io
 import sys
 import unittest
@@ -85,7 +72,7 @@ class TestSquare_instantiation(unittest.TestCase):
 
 
 class TestSquare_size(unittest.TestCase):
-    """Unittests for testing size initialization of the Square class."""
+    """Unittests for testing size ."""
 
     def test_None_size(self):
         with self.assertRaisesRegex(TypeError, "width must be an integer"):
@@ -234,7 +221,7 @@ class TestSquare_x(unittest.TestCase):
 
 
 class TestSquare_y(unittest.TestCase):
-    """Unittests for testing initialization of Square y attribute."""
+    """Unittests for testing  Square y attribute."""
 
     def test_None_y(self):
         with self.assertRaisesRegex(TypeError, "y must be an integer"):
@@ -318,7 +305,7 @@ class TestSquare_order_of_initialization(unittest.TestCase):
 
 
 class TestSquare_area(unittest.TestCase):
-    """Unittests for testing the area method of the Square class."""
+    """Unittests for testing the area method of the class."""
 
     def test_area_small(self):
         self.assertEqual(100, Square(10, 0, 0, 1).area())
