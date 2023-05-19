89e65b60ffee88e18da9afb944af915eb3bd8689
el898 <ebubeexcel3@gmail.com>
2023-05-19 05:29:27 +0100

    list priviledges

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,5 @@
--- creates the table force_name 
+-- Creates a table with a constraint on a column
+-- to not be null
 CREATE TABLE IF NOT EXISTS force_name(
     id INT,
     name VARCHAR(256) NOT NULL
