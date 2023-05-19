c772b439f3554f7e12fcea98538c44c2d2ed6cec
el898 <ebubeexcel3@gmail.com>
2023-05-19 05:33:58 +0100

    creates a table unique_id

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,7 +1,6 @@
--- Creates a table with a constraint on a column to have a unique value
--- for each row, which is never empty when a value isn't given
+-- Creates a table  unique_id
 CREATE TABLE IF NOT EXISTS unique_id(
     id INT DEFAULT 1,
     name VARCHAR(256),
     UNIQUE(id)
-);
+)
