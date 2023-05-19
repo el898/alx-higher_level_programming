1efaf005467dacfc52bcf326c5e5f99c0f261a5d
el898 <ebubeexcel3@gmail.com>
2023-05-19 06:02:39 +0100

    list all shows in the database

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
--- Prints and joins records from two tables with a matching field
+--  lists all shows contained in the database hbtn_0d_tvshow
 SELECT tv_shows.title, tv_show_genres.genre_id
     FROM tv_shows
     LEFT JOIN tv_show_genres
