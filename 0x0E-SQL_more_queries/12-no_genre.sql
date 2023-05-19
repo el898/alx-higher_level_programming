4e83e780c30b0640c1a98fa8059e6663aff21407
el898 <ebubeexcel3@gmail.com>
2023-05-19 06:01:25 +0100

    lists all shows contained in hbtn_0d_tvshows without a genre linked

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,4 @@
--- Prints and joins records from two tables with a matching field
+-- lists all shows contained in hbtn_0d_tvshows without a genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
     FROM tv_shows
     LEFT JOIN tv_show_genres
