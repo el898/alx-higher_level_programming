d180dc18008123bf433a6a471059e824b5301e4e
el898 <ebubeexcel3@gmail.com>
2023-05-19 05:39:36 +0100

    cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,4 @@
--- Creates a database that doesn't overwrite pre-existing versions
+-- Creates a database without overwriting existing versions
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Switches the active database
 USE hbtn_0d_usa;
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS cities(
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    UNIQUE(id),
+    UNIQUE(idd,
     FOREIGN KEY(state_id)
         REFERENCES states(id)
 );
