d5c00b0b3c24451f4cf40797384cde0c876db543
el898 <ebubeexcel3@gmail.com>
2023-05-19 05:44:52 +0100

    list all genres in the daTA BASE

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,4 +1,4 @@
--- Prints and joins records from two tables with a matching field
+-- lists all genres in the database hbtn_0d_tvshows_rate by their rating
 SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
     FROM tv_genres
     LEFT JOIN tv_show_genres
