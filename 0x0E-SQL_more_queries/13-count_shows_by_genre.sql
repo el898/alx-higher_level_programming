6751042e18d2c39893464bf054e02c431cec1053
el898 <ebubeexcel3@gmail.com>
2023-05-19 05:59:39 +0100

    task 13\

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,4 @@
--- Prints and joins records from two tables with a matching field
+--  lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
     FROM tv_genres
     RIGHT JOIN tv_show_genres
