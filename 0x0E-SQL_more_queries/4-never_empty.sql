cea2ba21669d77ca7d8ab1237fff6afcc13c181f
el898 <ebubeexcel3@gmail.com>
2023-05-19 05:32:13 +0100

    creates a table

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,6 +1,5 @@
--- Creates a table with a constraint on a column and another
--- column that is never empty when given a NULL value
+-- creates the table id_not_null on your MySQL server.
 CREATE TABLE IF NOT EXISTS id_not_null(
     id INT DEFAULT 1,
     name VARCHAR(256) NOT NULL
-);
+)
