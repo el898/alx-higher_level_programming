8e3279b61067601bee41218a52aa46864d38a633
el898 <ebubeexcel3@gmail.com>
2023-05-19 05:41:19 +0100

    cities

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,6 @@
--- Selects records from one table (cities) with a
--- column value that can be found in another set
+-- Selects records from a table (cities) with 
+-- column value  found in another set
+
 SELECT id, name FROM cities
     WHERE state_id IN
     (
